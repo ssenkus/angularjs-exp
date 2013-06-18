bd51e42d1f3baa4a1e153213c08212ee2c758444
Steven Senkus <steven.senkus@yahoo.com>
2013-06-17 21:08:37 -0700

    orders - now gets totals!

---
+++ experiments/order/scripts/order.js
@@ -0,0 +1,34 @@
+
+
+var app = angular.module('shopApp', []);       
+
+function CartController($scope) {
+        $scope.items = [
+                {title: 'Widget A', quantity: 8, price: 3.95},
+                {title: 'Widget B', quantity: 80, price: 1.45},
+                {title: 'Widget C', quantity: 18, price: 2.25}
+        ];
+        $scope.remove = function(index) {
+                $scope.items.splice(index, 1);
+
+        };
+
+        $scope.add = function(index) {
+                $scope.items.splice(index+1, 0,{title: 'Widget X', quantity: index+1, price: 13.95});
+
+        };
+
+        $scope.totalPrice = function() {
+            var total   = 0;
+                i       = 0;
+            for (i = 0; i < $scope.items.length; i++) {
+                total += ($scope.items[i].price * $scope.items[i].quantity);
+                
+            }
+            return total;
+        };
+
+}
+
+
+
