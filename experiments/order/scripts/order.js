8e7e2e7703b63202356c8666c3dca9882387b577
Steve <steven.senkus@yahoo.com>
2014-02-16 18:58:57 -0800

    REstructuring

--- experiments/order/scripts/order.js
+++ experiments/order/scripts/order.js
@@ -1,34 +1,42 @@
+var app = angular.module('shopApp', [])
+    .controller('CartController', function CartController($scope) {
+    $scope.items = [
+        {
+            title: 'Widget A',
+            quantity: 8,
+            price: 3.95
+        }, {
+            title: 'Widget B',
+            quantity: 80,
+            price: 1.45
+        },{
+            title: 'Widget C',
+            quantity: 18,
+            price: 2.25
+        }
+    ];
+    $scope.remove = function(index) {
+        $scope.items.splice(index, 1);
 
+    };
 
-var app = angular.module('shopApp', []);       
+    $scope.add = function(index) {
+        $scope.items.splice(index + 1, 0, {
+            title: 'Widget X',
+            quantity: index + 1,
+            price: 13.95
+        });
 
-function CartController($scope) {
-        $scope.items = [
-                {title: 'Widget A', quantity: 8, price: 3.95},
-                {title: 'Widget B', quantity: 80, price: 1.45},
-                {title: 'Widget C', quantity: 18, price: 2.25}
-        ];
-        $scope.remove = function(index) {
-                $scope.items.splice(index, 1);
+    };
 
-        };
+    $scope.totalPrice = function() {
+        var total = 0;
+        i = 0;
+        for (i = 0; i < $scope.items.length; i++) {
+            total += ($scope.items[i].price * $scope.items[i].quantity);
 
-        $scope.add = function(index) {
-                $scope.items.splice(index+1, 0,{title: 'Widget X', quantity: index+1, price: 13.95});
+        }
+        return total;
+    };
 
-        };
-
-        $scope.totalPrice = function() {
-            var total   = 0;
-                i       = 0;
-            for (i = 0; i < $scope.items.length; i++) {
-                total += ($scope.items[i].price * $scope.items[i].quantity);
-                
-            }
-            return total;
-        };
-
-}
-
-
-
+});
